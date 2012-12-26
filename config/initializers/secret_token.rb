2fa1d5c2bc8e573f80277d3a659c46770235aae5
Peng Jian <pengjian@snda.com>
2012-12-26 17:22:52 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ku6searchcharts::Application.config.secret_token = '68a4d6153495429da3ae1d3d8513f025b1e0519dbede5ed7469da008fdc8a32acfd34c6c4303f2bd32c65f39b3cbbb51a809b995d21acd57042c0a44e1bb4efa'
